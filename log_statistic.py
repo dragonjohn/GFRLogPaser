148571ef65d13b4d884b2f6ae1883b642b6f8beb
John Wu <dragon-john@yahoo.com.tw>
2015-03-25 15:50:20 +0800

    Update log_statistic.py

--- log_statistic.py
+++ log_statistic.py
@@ -11,15 +11,19 @@ if len(sys.argv)>1:
 	file = open(sys.argv[1],'r')
 	for line in file.readlines():
 		if line.find(" decode time: ")>0:
-			if line[:19] in timeList:
-				idx = timeList.index(line[:19])
+			if line[11:19] in timeList:
+				idx = timeList.index(line[11:19])
 				timeCount[idx] = timeCount[idx]+1
 			else:
 				# add to time list
-				timeList.append(line[:19])
+				timeList.append(line[11:19])
 				timeCount.append(1)
 	file.close()
-	
+	file = open('statistic.log','w')
+	for idx, item in enumerate(timeList):
+		file.write(str(idx)+' '+item+' '+str(timeCount[idx])+'\n')
+	file.close()
+
 if timeCount:
 	peak = timeCount[0]
 	nadir = timeCount[0]
@@ -27,9 +31,6 @@ if timeCount:
 		if item > peak:
 			peak = item
 			peakTime = timeList[idx]
-		if item < nadir:
-			nadir = item
-			nadirTime = timeList[idx]
 		average+=item
 
-print 'Peak time:'+peakTime+'('+str(peak)+'), Nadir time: '+nadirTime+'('+str(nadir)+'), Average:('+str(float(average/len(timeCount)))+')'
+print 'Peak time:'+peakTime+'('+str(peak)+'), Average:('+str(float(average/len(timeCount)))+')'
